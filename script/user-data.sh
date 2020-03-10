800ed3c094972420ee250be593e647527e9388ef
ganez-bhoosal <52504583+ganez-bhoosal@users.noreply.github.com>
2020-03-10 19:50:18 -0400

    Update user-data.sh

--- script/user-data.sh
+++ script/user-data.sh
@@ -1,1 +1,28 @@
+vol=vol-0b52b8f26ac4dcda9
+mount_dir=/ganesh
+device=/dev/xvdf/
+
+if [ ! -d $mount_dir ]
+then 
+mkdir $mount_dir 
+fi 
+
+echo "this command is to fetch instance id"
+instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
+
+
+echo "this command is to attach EBS to EC2"
+aws ec2 attach-volume --volume-id $vol --instance-id $instance_id --device /dev/sdf
+
+sleep 60 
+
+echo "mount filesystem to folder" 
+mount $device $mount_dir
+
+
+
+
+
+
+
 
