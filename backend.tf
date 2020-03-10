250889226d89324df18a004cbb35f78efa4a706f
rupesh2010 <rupesh.phuyal1986@gmail.com>
2020-03-09 20:28:50 -0400

    added new files

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-ganesh123"
+    key    = "terraform/dev/ebs"
+    region = "us-east-1"
+    profile = "ganesh"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_launch_configuration" "default" {
+  name          = "terraform-ebs"
+  image_id      = "ami-04ff587bc1c1d65e6"
+  instance_type = "t2.micro"
+  key_name = "terraform-ebs"
+  security_groups = ["${aws_security_group.default.id}"]
+  iam_instance_profile = "${aws_iam_instance_profile.default.name}"
+  user_data = "${data.template_file.default.rendered}"
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "template_file" "default" {
+  template = "${file("script/user-data.sh")}"
+  
+  
+}
